d9133a5b632410163fc9bb869959fdf1a7728757
Sebastien Varlet <sebastien.varlet@gmail.com>
2020-09-17 00:30:07 +0100

    Refactor magic strings used by the renderer to fence the canvas.

--- lib/termpaint.ex
+++ lib/termpaint.ex
@@ -1,4 +1,8 @@
 defmodule Termpaint do
+  @vertical_fencing_char "-"
+  @horizontal_fencing_char "|"
+  @blank_char " "
+
   def process_command("C" <> dimensions) do
     {width, height} = parse_dimensions(dimensions)
     header = render_vertical_fencing(width)
@@ -15,11 +19,23 @@ defmodule Termpaint do
   end
 
   defp render_vertical_fencing(width) do
-    "-#{String.duplicate("-", width)}-\n"
+    @vertical_fencing_char
+    |> String.duplicate(width)
+    |> surround(@vertical_fencing_char)
+    |> newline()
   end
 
   defp render_row(width) do
-    "|#{String.duplicate(" ", width)}|\n"
+    @blank_char
+    |> String.duplicate(width)
+    |> surround(@horizontal_fencing_char)
+    |> newline()
   end
+
+  defp surround(string, padding) do
+    "#{padding}#{string}#{padding}"
+  end
+
+  defp newline(string), do: "#{string}\n"
 
 end
