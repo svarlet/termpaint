e0123df294318e2c06b53c9ae2a7683c8ed7a4e6
Sebastien Varlet <sebastien.varlet@gmail.com>
2020-09-17 00:14:30 +0100

    Extract the dimensions parser to a parse_dimensions function.

--- lib/termpaint.ex
+++ lib/termpaint.ex
@@ -1,12 +1,17 @@
 defmodule Termpaint do
   def process_command("C" <> dimensions) do
-    %{"width" => width, "height" => height} = Regex.named_captures(~r/(?<width>\d+)\ +(?<height>\d+)/, dimensions)
-    {width, _} = Integer.parse(width)
-    {height, _} = Integer.parse(height)
+    {width, height} = parse_dimensions(dimensions)
     header = render_vertical_fencing(width)
     body = List.duplicate(render_row(width), height)
     footer = render_vertical_fencing(width)
     Enum.join([header, body, footer])
+  end
+
+  defp parse_dimensions(text) do
+    %{"width" => width, "height" => height} = Regex.named_captures(~r/(?<width>\d+)\ +(?<height>\d+)/, text)
+    {width, _} = Integer.parse(width)
+    {height, _} = Integer.parse(height)
+    {width, height}
   end
 
   defp render_vertical_fencing(width) do
