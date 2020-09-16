ddd9431c420c61396a9f7dbcd8406b4e8a874f0e
Sebastien Varlet <sebastien.varlet@gmail.com>
2020-09-17 00:10:15 +0100

    Replace render_header and render_footer by render_vertical_fencing.

--- lib/termpaint.ex
+++ lib/termpaint.ex
@@ -3,17 +3,15 @@ defmodule Termpaint do
     %{"width" => width, "height" => height} = Regex.named_captures(~r/(?<width>\d+)\ +(?<height>\d+)/, dimensions)
     {width, _} = Integer.parse(width)
     {height, _} = Integer.parse(height)
-    header = render_header(width)
+    header = render_vertical_fencing(width)
     body = List.duplicate(render_row(width), height)
-    footer = render_footer(width)
+    footer = render_vertical_fencing(width)
     Enum.join([header, body, footer])
   end
 
-  defp render_header(width) do
+  defp render_vertical_fencing(width) do
     "-#{String.duplicate("-", width)}-\n"
   end
-
-  defp render_footer(width), do: "-#{String.duplicate("-", width)}-\n"
 
   defp render_row(width), do: "|#{String.duplicate(" ", width)}|\n"
 
