f88f82b0eebaab98aae6aa936f6c7049cdd4b430
Sebastien Varlet <sebastien.varlet@gmail.com>
2020-09-17 21:04:30 +0100

    Add a missing test for the creation and output of a canvas.

--- test/termpaint_test.exs
+++ test/termpaint_test.exs
@@ -5,12 +5,23 @@ defmodule TermpaintTest do
 
   alias Termpaint.Canvas
 
-  setup do
-    [state: nil]
+  test "create a 4x4 canvas" do
+    state = nil
+
+    assert capture_io(fn -> Termpaint.process_command(state, "C 4 4") end) ==
+             """
+             ------
+             |    |
+             |    |
+             |    |
+             |    |
+             ------
+             """
   end
 
   test "draw a line from (1,1) to (3,3) in a 3x3 canvas" do
     state = Canvas.new(3, 3)
+
     assert capture_io(fn -> Termpaint.process_command(state, "L 1 1 3 3") end) ==
              """
              -----
@@ -20,5 +31,4 @@ defmodule TermpaintTest do
              -----
              """
   end
-
 end
