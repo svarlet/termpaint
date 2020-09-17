19897c34beb76ca1d707aba78d5f27952b2a40e8
Sebastien Varlet <sebastien.varlet@gmail.com>
2020-09-17 16:20:01 +0100

    Decoupled a test from the canvas API to verify the renderer is ink agnostic.

--- test/renderer_test.exs
+++ test/renderer_test.exs
@@ -65,17 +65,23 @@ defmodule Termpaint.RendererTest do
              """
   end
 
-  test "3x3 canvas with a line from {1,1} to {3,3}" do
-    canvas =
-      Canvas.new(3, 3)
-      |> Canvas.draw_line(1, 1, 3, 3)
+  test "3x3 canvas with various spots" do
+    canvas = Canvas.new(3, 3)
+    marks = %{
+      {1, 1} => "x",
+      {2, 1} => "0",
+      {3, 2} => "9",
+      {1, 2} => "S",
+      {3, 1} => ".",
+    }
 
+    canvas = %Canvas{canvas | coords: marks}
     assert Renderer.render_canvas(canvas) ==
              """
              -----
-             |xxx|
-             |  x|
-             |  x|
+             |x0.|
+             |S 9|
+             |   |
              -----
              """
   end
