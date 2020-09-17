9718fa826a1e1995d287c5c4063eb0479150505a
Sebastien Varlet <sebastien.varlet@gmail.com>
2020-09-17 23:10:57 +0100

    Moved parser functions from main module to their own dedicated module.

--- lib/termpaint.ex
+++ lib/termpaint.ex
@@ -1,6 +1,8 @@
 defmodule Termpaint do
   alias Termpaint.{Canvas, Renderer}
 
+  import Termpaint.Parser
+
   def main(_args) do
     read_command(nil)
   end
@@ -47,34 +49,5 @@ defmodule Termpaint do
     |> IO.write()
   end
 
-  defp parse_fill_arguments(text) do
-    %{"x" => x, "y" => y, "ink" => ink} =
-      Regex.named_captures(~r/(?<x>\d+)\ +(?<y>\d+)\ +(?<ink>.)/, text)
 
-    {x, _} = Integer.parse(x)
-    {y, _} = Integer.parse(y)
-
-    {x, y, ink}
-  end
-
-  defp parse_vector(text) do
-    %{"x1" => x1, "y1" => y1, "x2" => x2, "y2" => y2} =
-      Regex.named_captures(~r/(?<x1>\d+)\ +(?<y1>\d+)\ +(?<x2>\d+)\ +(?<y2>\d+)/, text)
-
-    {x1, _} = Integer.parse(x1)
-    {y1, _} = Integer.parse(y1)
-    {x2, _} = Integer.parse(x2)
-    {y2, _} = Integer.parse(y2)
-
-    {x1, y1, x2, y2}
-  end
-
-  defp parse_dimensions(text) do
-    %{"width" => width, "height" => height} =
-      Regex.named_captures(~r/(?<width>\d+)\ +(?<height>\d+)/, text)
-
-    {width, _} = Integer.parse(width)
-    {height, _} = Integer.parse(height)
-    {width, height}
-  end
 end
