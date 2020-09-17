5d3303feb78091cad1925522991ed09e5dc1eea0
Sebastien Varlet <sebastien.varlet@gmail.com>
2020-09-17 10:44:46 +0100

    We can create a Canvas value of any dimension.

--- lib/termpaint.ex
+++ lib/termpaint.ex
@@ -11,7 +11,7 @@ defmodule Termpaint do
     defstruct width: 0, height: 0
 
     def new(width, height) do
-      %__MODULE__{width: 1, height: 1}
+      %__MODULE__{width: width, height: height}
     end
   end
 
--- test/canvas_test.exs
+++ test/canvas_test.exs
@@ -4,6 +4,10 @@ defmodule Termpaint.CanvasTest do
   alias Termpaint.Canvas
 
   test "a new 1x1 canvas" do
-    assert %Canvas{width: 1, height: 1} == Canvas.new(1,1)
+    assert %Canvas{width: 1, height: 1} == Canvas.new(1, 1)
+  end
+
+  test "a new 2x7 canvas" do
+    assert %Canvas{width: 2, height: 7} == Canvas.new(2, 7)
   end
 end
