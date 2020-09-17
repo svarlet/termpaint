8dd296bcfcfad83c59957e114b3477e21ff46519
Sebastien Varlet <sebastien.varlet@gmail.com>
2020-09-17 11:15:59 +0100

    Extract a Renderer module.

--- lib/termpaint.ex
+++ lib/termpaint.ex
@@ -1,8 +1,4 @@
 defmodule Termpaint do
-  @vertical_fencing_char "-"
-  @horizontal_fencing_char "|"
-  @blank_char " "
-
   defmodule State do
     defstruct canvas: nil
   end
@@ -15,43 +11,56 @@ defmodule Termpaint do
     end
   end
 
+  defmodule Renderer do
+    @vertical_fencing_char "-"
+    @horizontal_fencing_char "|"
+    @blank_char " "
+
+    def render_canvas(canvas) do
+      header = render_vertical_fencing(canvas.width)
+      body = List.duplicate(render_row(canvas.width), canvas.height)
+      footer = render_vertical_fencing(canvas.width)
+      Enum.join([header, body, footer])
+    end
+
+    defp render_vertical_fencing(width) do
+      @vertical_fencing_char
+      |> String.duplicate(width)
+      |> surround(@vertical_fencing_char)
+      |> newline()
+    end
+
+    defp render_row(width) do
+      @blank_char
+      |> String.duplicate(width)
+      |> surround(@horizontal_fencing_char)
+      |> newline()
+    end
+
+    defp surround(string, padding) do
+      "#{padding}#{string}#{padding}"
+    end
+
+    defp newline(string), do: "#{string}\n"
+  end
+
   def new() do
     %Termpaint.State{}
   end
 
   def process_command(_state, "C" <> dimensions) do
     {width, height} = parse_dimensions(dimensions)
-    header = render_vertical_fencing(width)
-    body = List.duplicate(render_row(width), height)
-    footer = render_vertical_fencing(width)
-    Enum.join([header, body, footer])
+
+    Canvas.new(width, height)
+    |> Renderer.render_canvas()
   end
 
   defp parse_dimensions(text) do
-    %{"width" => width, "height" => height} = Regex.named_captures(~r/(?<width>\d+)\ +(?<height>\d+)/, text)
+    %{"width" => width, "height" => height} =
+      Regex.named_captures(~r/(?<width>\d+)\ +(?<height>\d+)/, text)
+
     {width, _} = Integer.parse(width)
     {height, _} = Integer.parse(height)
     {width, height}
   end
-
-  defp render_vertical_fencing(width) do
-    @vertical_fencing_char
-    |> String.duplicate(width)
-    |> surround(@vertical_fencing_char)
-    |> newline()
-  end
-
-  defp render_row(width) do
-    @blank_char
-    |> String.duplicate(width)
-    |> surround(@horizontal_fencing_char)
-    |> newline()
-  end
-
-  defp surround(string, padding) do
-    "#{padding}#{string}#{padding}"
-  end
-
-  defp newline(string), do: "#{string}\n"
-
 end
