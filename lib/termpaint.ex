d4bff9e691c04f69c2378b09285bf5e6099663aa
Sebastien Varlet <sebastien.varlet@gmail.com>
2020-09-19 11:11:08 +0100

    Restart from scratch.

--- lib/termpaint.ex
+++ lib/termpaint.ex
@@ -1,17 +1,2 @@
 defmodule Termpaint do
-  alias Termpaint.{Printer, CommandInterpreter}
-
-  @prompt "enter command: "
-
-  def main(_args) do
-    Stream.unfold(nil, &run/1)
-    |> Stream.run()
-  end
-
-  defp run(state) do
-    command = IO.gets(@prompt)
-    new_state = CommandInterpreter.process_command(state, command)
-    Printer.print_canvas(new_state)
-    {state, new_state}
-  end
 end
