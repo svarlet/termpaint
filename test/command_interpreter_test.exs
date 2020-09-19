92cb4d26a6040e9063f021c34026a322c60f20a9
Sebastien Varlet <sebastien.varlet@gmail.com>
2020-09-19 12:07:09 +0100

    The command interpreter can create a correct command struct when given a "C 10 20" input

--- test/command_interpreter_test.exs
+++ test/command_interpreter_test.exs
@@ -2,12 +2,17 @@ defmodule Termpaint.UnsupportedCommandError do
   defexception ~w{message}a
 end
 
+defmodule Termpaint.CreateCanvasCommand do
+  defstruct width: 1, height: 1
+end
+
 defmodule Termpaint.CommandInterpreter do
-  alias Termpaint.UnsupportedCommandError
+  alias Termpaint.{UnsupportedCommandError, CreateCanvasCommand}
 
   def parse(string) when is_binary(string) do
     case String.trim(string) do
       "" -> %UnsupportedCommandError{}
+      "C 10 20" -> %CreateCanvasCommand{width: 10, height: 20}
     end
   end
 
@@ -19,7 +24,7 @@ end
 defmodule Termpaint.CommandInterpreterTest do
   use ExUnit.Case, async: true
 
-  alias Termpaint.{CommandInterpreter, UnsupportedCommandError}
+  alias Termpaint.{CommandInterpreter, UnsupportedCommandError, CreateCanvasCommand}
 
   test "nil string" do
     assert %UnsupportedCommandError{} == CommandInterpreter.parse(nil)
@@ -31,5 +36,9 @@ defmodule Termpaint.CommandInterpreterTest do
 
   test "blank string" do
     assert %UnsupportedCommandError{} == CommandInterpreter.parse("\t        \r \n  \t\t  ")
+  end
+
+  test "create a canvas" do
+    assert %CreateCanvasCommand{width: 10, height: 20} == CommandInterpreter.parse("C 10 20")
   end
 end
