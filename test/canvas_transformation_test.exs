17930bf4a4aac3ee846976412019435a5b85a549
Sebastien Varlet <sebastien.varlet@gmail.com>
2020-09-19 16:39:27 +0100

    Creation of a 1x1 canvas based on a nil canvas returns a 1x1 canvas

--- test/canvas_transformation_test.exs
+++ test/canvas_transformation_test.exs
@@ -1,10 +1,15 @@
 defmodule CanvasTransformationTest do
   use ExUnit.Case, async: true
 
-  alias Termpaint.{CreateCanvasCommand, AbsurdCanvasSizeError, CanvasTransformation}
+  alias Termpaint.{CreateCanvasCommand, AbsurdCanvasSizeError, CanvasTransformation, Canvas}
 
   test "creating a 0x0 canvas is an error" do
     command = %CreateCanvasCommand{width: 0, height: 0}
     assert %AbsurdCanvasSizeError{} == CanvasTransformation.transform(command, nil)
+  end
+
+  test "given a nil canvas, creating a 1x1 canvas returns a 1x1 canvas" do
+    command = %CreateCanvasCommand{width: 1, height: 1}
+    assert %Canvas{width: 1, height: 1} == CanvasTransformation.transform(command, nil)
   end
 end
