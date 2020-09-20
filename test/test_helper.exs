0e8a250514987fd5969482db476828271059a9db
Sebastien Varlet <sebastien.varlet@gmail.com>
2020-09-20 15:05:44 +0100

    Move the definition of assert_coords_marked to a new TestHelpers module to enable the extration of line tests to a new dedicated module

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,10 @@
 ExUnit.start()
+
+defmodule Termpaint.TestHelpers do
+  import ExUnit.Assertions
+
+  def assert_coords_marked(bitmap, coords) do
+    expected_bitmap = for coord <- coords, into: %{}, do: {coord, "x"}
+    assert expected_bitmap == bitmap
+  end
+end
