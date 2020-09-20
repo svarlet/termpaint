9a9c4ce6ff7fcd7894c399c1d0bb90c7f3a6526d
Sebastien Varlet <sebastien.varlet@gmail.com>
2020-09-20 17:57:08 +0100

    TextRenderer relies on IO List for maximum rendering performance instead of repeated string concats

--- lib/termpaint/text_renderer.ex
+++ lib/termpaint/text_renderer.ex
@@ -1,9 +1,15 @@
 defmodule Termpaint.TextRenderer do
-  def render(_canvas) do
-    """
-    ---
-    |.|
-    ---
-    """
+  def render(canvas) do
+    header = "---\n"
+    footer = header
+    body =
+      for y <- 1..canvas.height do
+        row =
+          for x <- 1..canvas.width do
+            canvas.bitmap[{x, y}] || " "
+          end
+        ["|" | List.insert_at(row, -1, "|\n")]
+      end
+    [header, body, footer]
   end
 end
