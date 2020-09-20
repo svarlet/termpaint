b13da7dc72da52694123e186a30e5f4a5c23b873
Sebastien Varlet <sebastien.varlet@gmail.com>
2020-09-20 18:33:10 +0100

    Main loop

--- lib/termpaint.ex
+++ lib/termpaint.ex
@@ -1,2 +1,34 @@
 defmodule Termpaint do
+  alias Termpaint.{
+    CommandInterpreter,
+    CanvasTransformation,
+    TextRenderer
+  }
+
+  def main(_args) do
+    nil
+    |> Stream.unfold(&run/1)
+    |> Stream.run()
+  end
+
+  defp run(app_state) do
+    new_app_state =
+      IO.gets("enter command: ")
+      |> CommandInterpreter.parse()
+      |> CanvasTransformation.transform(app_state)
+      |> tee(&output/1)
+
+    {app_state, new_app_state}
+  end
+
+  defp output(app_state) do
+    app_state
+    |> TextRenderer.render()
+    |> IO.write()
+  end
+
+  defp tee(value, fun) do
+    fun.(value)
+    value
+  end
 end
