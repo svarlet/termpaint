86bdf1ded0b2480ab3d8182b7f70ec4e3b654582
Sebastien Varlet <sebastien.varlet@gmail.com>
2020-09-20 11:43:17 +0100

    We can draw a diagonal to the top right corner

--- test/canvas_transformation_test.exs
+++ test/canvas_transformation_test.exs
@@ -88,59 +88,49 @@ defmodule CanvasTransformationTest do
     test "draw a 1px line", context do
       a_1px_line_command = %DrawLineCommand{from: {2, 3}, to: {2, 3}}
       canvas = CanvasTransformation.transform(a_1px_line_command, context.a_3x3_canvas)
-      assert %{{2, 3} => "x"} == canvas.bitmap
+      assert_coords_marked(canvas.bitmap, [{2, 3}])
     end
 
     test "draw a horizontal line", context do
       a_hline_command = %DrawLineCommand{from: {1, 2}, to: {3, 2}}
       canvas = CanvasTransformation.transform(a_hline_command, context.a_3x3_canvas)
-      assert %{
-        {1, 2} => "x",
-        {2, 2} => "x",
-        {3, 2} => "x"
-      } == canvas.bitmap
+      assert_coords_marked(canvas.bitmap, [{1, 2}, {2, 2}, {3, 2}])
     end
 
     test "draw a vertical line", context do
       a_vline_command = %DrawLineCommand{from: {2, 1}, to: {2, 3}}
       canvas = CanvasTransformation.transform(a_vline_command, context.a_3x3_canvas)
-      assert %{
-        {2, 1} => "x",
-        {2, 2} => "x",
-        {2, 3} => "x"
-      } == canvas.bitmap
+      assert_coords_marked(canvas.bitmap, [{2, 1}, {2, 2}, {2, 3}])
     end
 
     test "a diagonal line is drawn as a horizontal line and a vertical line", context do
       a_diagonal_command = %DrawLineCommand{from: {1, 1}, to: {3, 3}}
       canvas = CanvasTransformation.transform(a_diagonal_command, context.a_3x3_canvas)
-      assert %{
-        {1, 1} => "x",
-        {2, 1} => "x",
-        {3, 1} => "x",
-        {3, 2} => "x",
-        {3, 3} => "x"
-      } = canvas.bitmap
+      assert_coords_marked(canvas.bitmap, [{1, 1}, {2, 1}, {3, 1}, {3, 2}, {3, 3}])
     end
 
     test "diagonal toward the top left corner", context do
       a_diagonal_command = %DrawLineCommand{from: {2, 3}, to: {1, 2}}
       canvas = CanvasTransformation.transform(a_diagonal_command, context.a_3x3_canvas)
-      assert %{
-        {1, 2} => "x",
-        {1, 3} => "x",
-        {2, 3} => "x"
-      } == canvas.bitmap
+      assert_coords_marked(canvas.bitmap, [{1, 2}, {1, 3}, {2, 3}])
     end
 
     test "diagonal toward the bottom left corner", context do
       a_diagonal_command = %DrawLineCommand{from: {3, 1}, to: {2, 2}}
       canvas = CanvasTransformation.transform(a_diagonal_command, context.a_3x3_canvas)
-      assert %{
-        {3, 1} => "x",
-        {2, 1} => "x",
-        {2, 2} => "x"
-      } = canvas.bitmap
+      assert_coords_marked(canvas.bitmap, [{3, 1}, {2, 1}, {2, 2}])
+    end
+
+    test "a diagonal towards the top right corner", context do
+      a_diagonal_command = %DrawLineCommand{from: {1, 3}, to: {3, 2}}
+      canvas = CanvasTransformation.transform(a_diagonal_command, context.a_3x3_canvas)
+      assert_coords_marked(canvas.bitmap, [{1, 3}, {2, 3}, {3, 3}, {3, 2}])
+    end
+
+    defp assert_coords_marked(bitmap, coords) do
+      for coord <- coords do
+        assert(bitmap[coord] == "x", "Expected #{inspect(coord)} to be marked.")
+      end
     end
   end
 end
